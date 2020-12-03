b7792b073a4d641ed6beed01aefebed9842c8aea
unknown <nituldeori63@gmail.com>
2020-12-04 00:38:29 +0530

    Added: Extend the max method to take more than 3 parameters

--- Generics.java
+++ Generics.java
@@ -1,6 +1,6 @@
 package com.findmaximumgenerics;
 
-public class Generics {
+public class Generics{
     public Integer findMaxInt(Integer[] A){
         Integer max=A[0];
         for(int i=1;i<A.length;i++){
@@ -26,6 +26,16 @@ public class Generics {
         for(int i=1;i<C.length;i++){
             if(C[i].compareTo(max)>0){
                 max=C[i];
+            }
+        }
+        return max;
+    }
+
+    public <K extends Comparable> K findMax(K[] D){
+        K max=D[0];
+        for(int i=1;i<D.length;i++){
+            if(D[i].compareTo(max)>0){
+                max=D[i];
             }
         }
         return max;
--- GenericsTest.java
+++ GenericsTest.java
@@ -9,34 +9,34 @@ public class GenericsTest {
     @Test
     public void given3IntegersShouldReturnMaximum(){
         Generics g=new Generics();
-        Integer[] A={3,2,1};
-        Assert.assertEquals(A[0],g.findMaxInt(A));
-        Integer[] B={3,9,5};
-        Assert.assertEquals(B[1],g.findMaxInt(B));
+        Integer[] A={3,2,1,1};
+        Assert.assertEquals(A[0],g.findMax(A));
+        Integer[] B={3,9,5,4};
+        Assert.assertEquals(B[1],g.findMax(B));
         Integer[] C={2,6,10};
-        Assert.assertEquals(C[2], g.findMaxInt(C));
+        Assert.assertEquals(C[2], g.findMax(C));
     }
 
     @Test
     public void given3FloatsShouldReturnMaximum(){
         Generics g=new Generics();
-        Float[] A={3.2f,2.1f,1.8f};
-        Assert.assertEquals(A[0],g.findMaxFloat(A));
+        Float[] A={3.2f,2.1f,1.8f,3.1f,2.9f};
+        Assert.assertEquals(A[0],g.findMax(A));
         Float[] B={3.5f,9.1f,5.7f};
-        Assert.assertEquals(B[1],g.findMaxFloat(B));
-        Float[] C={2.8f,6.1f,10.5f};
-        Assert.assertEquals(C[2], g.findMaxFloat(C));
+        Assert.assertEquals(B[1],g.findMax(B));
+        Float[] C={2.8f,6.1f,10.5f,4.5f,3.4f};
+        Assert.assertEquals(C[2], g.findMax(C));
     }
 
     @Test
     public void given3StringsShouldReturnMaximum(){
         Generics g=new Generics();
-        String[] A={"Nitul","Hayato","Alok"};
-        Assert.assertEquals(A[0],g.findMaxString(A));
-        String[] B={"Misa","Shaini","Kshmir"};
-        Assert.assertEquals(B[1],g.findMaxString(B));
+        String[] A={"Nitul","Hayato","Alok","Moko"};
+        Assert.assertEquals(A[0],g.findMax(A));
+        String[] B={"Misa","Shaini","Kshmir","Miguel"};
+        Assert.assertEquals(B[1],g.findMax(B));
         String[] C={"Adam","Eve","Jay"};
-        Assert.assertEquals(C[2], g.findMaxString(C));
+        Assert.assertEquals(C[2], g.findMax(C));
     }
 
 
